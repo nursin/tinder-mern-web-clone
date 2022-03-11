08d77cf97803e7a6454d99cf53a81d02e50edfb7
Bobby Keel <bobbykeel76@gmail.com>
2022-03-11 04:01:44 -0600

    swipeButtons

--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import "./SwipeButtons.css";
+import ReplayIcon from '@mui/icons-material/Replay';
+import CloseIcon from '@mui/icons-material/Close';
+import StarIcon from '@mui/icons-material/Star';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FlashIcon from '@mui/icons-material/FlashOn';
+import IconButton from '@mui/material/IconButton';
 
 function SwipeButtons() {
   return (
     <div className='swipeButtons'>
-
+        <IconButton className='swipeButtons__repeat'>
+            <ReplayIcon fontSize='large'/>
+        </IconButton>
+        <IconButton className='swipeButtons__left'>
+            <CloseIcon fontSize='large'/>
+        </IconButton>
+        <IconButton className='swipeButtons__star'>
+            <StarIcon fontSize='large'/>
+        </IconButton>
+        <IconButton className='swipeButtons__right'>
+            <FavoriteIcon fontSize='large'/>
+        </IconButton>
+        <IconButton className='swipeButtons__lightning'>
+            <FlashIcon fontSize='large'/>
+        </IconButton>
     </div>
   )
 }
