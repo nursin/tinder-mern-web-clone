76912b49d8c3840a52de2af27f864765752d1db1
Bobby Keel <bobbykeel76@gmail.com>
2022-03-11 07:06:07 -0600

    done

--- tinder-mern-frontend/src/TinderCards.js
+++ tinder-mern-frontend/src/TinderCards.js
@@ -1,20 +1,23 @@
 import { SwipeDown } from '@mui/icons-material';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TinderCard from 'react-tinder-card';
 import "./TinderCards.css";
+import axios from './axios';
 
 function TinderCards() {
-    const [people, setPeople] = useState([
-        {
-            name: "Elon Musk",
-            url: "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
-        },
-        {
-            name: "Jeff Bezos",
-            url: "https://m.media-amazon.com/images/M/MV5BYTNlOGZhYzgtMmE3OC00Y2NiLWFhNWQtNzg5MjRhNTJhZGVmXkEyXkFqcGdeQXVyNzg5MzIyOA@@._V1_.jpg",
-        },
-    ]);
+    const [people, setPeople] = useState([]);
 
+    useEffect(() => {
+        async function fetchData() {
+            const req = await axios.get('/tinder/cards');
+
+            setPeople(req.data);
+        }
+
+        fetchData();
+    }, [])
+
+    console.log(people)
     const swiped = (direction, nameToDelete) => {
         console.log("removing", nameToDelete);
         // setLastDirection(direction);
@@ -31,11 +34,11 @@ function TinderCards() {
                         className='swipe'
                         key={person.name}
                         preventSwipe={["up", "down"]}
-                        onSwipe={(dir) => SwipeDown(dir, person.name)}
+                        onSwipe={(dir) => swiped(dir, person.name)}
                         onCardLeftScreen={() => outOfFrame(person.name)}
                     >
                         <div
-                            style={{ backgroundImage: `url(${person.url})` }}
+                            style={{ backgroundImage: `url(${person.imgUrl})` }}
                             className="card"
                         >
                             <h3>{person.name}</h3>
