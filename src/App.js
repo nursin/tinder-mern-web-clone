f663824234b5c7c0412c826d06bb043088459811
Bobby Keel <bobbykeel76@gmail.com>
2022-03-11 03:39:33 -0600

    tinderCards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header';
+import SwipeButtons from './SwipeButtons';
+import TinderCards from './TinderCards';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* TinderCards */}
-      {/* SwipeButtons */}
+      <TinderCards />
+      <SwipeButtons />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PersonIcon from '@mui/icons-material/Person';
+import ForumIcon from '@mui/icons-material/Forum';
 import IconButton from '@mui/material/IconButton';
 import './Header.css'
 
@@ -17,8 +18,15 @@ function Header() {
         src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-logo.png"
         alt=''
       />
+      <IconButton>
+        <ForumIcon
+          fontSize='large'
+          className='header__icon'
+        />
+      </IconButton>
     </div>
   )
 }
+
 
 export default Header
